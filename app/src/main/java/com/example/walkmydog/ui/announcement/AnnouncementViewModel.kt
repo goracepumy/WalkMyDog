de119473c3d9cc7519fce3c5b4c7bd1c7fc460db
lukaszbrylka <lukaszbrylk@outlook.com>
2021-12-02 17:59:12 +0100

    test1

--- app/src/main/java/com/example/walkmydog/ui/announcement/AnnouncementViewModel.kt
+++ app/src/main/java/com/example/walkmydog/ui/announcement/AnnouncementViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.walkmydog.ui.dashboard
+package com.example.walkmydog.ui.announcement
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class AnnouncementViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
--- app/src/main/java/com/example/walkmydog/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/walkmydog/ui/search/SearchFragment.kt
@@ -1,4 +1,5 @@
-package com.example.walkmydog.ui.dashboard
+package com.example.walkmydog.ui.search
+
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +9,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.walkmydog.R
-import com.example.walkmydog.databinding.FragmentDashboardBinding
+import com.example.walkmydog.databinding.FragmentSearchBinding
 
-class DashboardFragment : Fragment() {
+class SearchFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+    private lateinit var searchViewModel: SearchViewModel
+    private var _binding: FragmentSearchBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +25,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        searchViewModel =
+            ViewModelProvider(this).get(SearchViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textNotifications
+        searchViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
